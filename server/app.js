085fcac85a0b60fa234fc2f1a578fb1f7f96d264
kamran <kamransadiq772@gmail.com>
2022-10-27 13:47:19 +0500

    dd

--- server/app.js
+++ server/app.js
@@ -9,11 +9,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 
 
 
